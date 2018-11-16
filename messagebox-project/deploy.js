b20f83550f84e1038f3d143a649367120d896ec8
Nikhil Padala <padalan@gmail.com>
2018-11-16 16:01:48 -0500

    Beginner notes, corrected the grammar

--- messagebox-project/deploy.js
+++ messagebox-project/deploy.js
@@ -9,10 +9,10 @@ const provider = new HDWalletProvider(
   'https://rinkeby.infura.io/v3/34e7998b9dce4f5491c31485ab6c8c4b'
 );
 
+const web3 = new Web3(provider);
 
 // We don't need to write a function below but we do it
 // as we cannot write await without a function.
-const web3 = new Web3(provider);
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
   console.log('Attempting to deploy from account', accounts[0]);
